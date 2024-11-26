85469a989424bde65d2a9d367882377c3f1d6b45
Vicky Kumar <vyadav8518@gmail.com>
2024-11-26 21:31:34 +0530

    initial commit

---
+++ welcome-app/src/main/java/com/example/welcome_app/WelcomeAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.welcome_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomeAppApplication.class, args);
+	}
+
+}
---
+++ welcome-app/src/test/java/com/example/welcome_app/WelcomeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.welcome_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelcomeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
